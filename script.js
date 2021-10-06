6590c9b38165d4d506ef162bb1d80f47c1a9a4f8
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-10-06 18:09:26 -0500

    added current/duration and progress bar seek.

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
-
+const video = document.querySelector('video');
+const progressRanges = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRanges = document.querySelector('.volume-range');
+const volumeBars = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullScreen = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+const showPlayIcon = () => {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+};
 
+const togglePlay = () => {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+};
 
+// On Video End, show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+const displayTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+};
 
+// Update progress bar as the video is playing
+const updateProgress = () => {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+};
+
+// Click to seek
+const setProgress = (e) => {
+  const newTime = e.offsetX / progressRanges.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+};
 
 // Volume Controls --------------------------- //
 
-
-
 // Change Playback Speed -------------------- //
-
-
 
 // Fullscreen ------------------------------- //
 
-
+// Event Listeners --------------------------------------------------------
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRanges.addEventListener('click', setProgress);
